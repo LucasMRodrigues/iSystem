d711e48c7633741ecb5e15fd2138eecf97cf7e97
Lucas <lucasmrodrigues12@gmail.com>
2020-02-27 10:41:25 -0300

    Initial Commit

---
+++ baixa-de-os/baixa-de-os-service/src/main/java/com/net/isystem/baixadeos/BaixaDeOsApplication.java
@@ -0,0 +1,13 @@
+package com.net.isystem.baixadeos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaixaDeOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaixaDeOsApplication.class, args);
+	}
+
+}
---
+++ baixa-de-os/baixa-de-os-service/src/test/java/com/net/isystem/baixadeos/BaixaDeOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.isystem.baixadeos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaixaDeOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
